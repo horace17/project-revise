a5933acf2156fef4b46e8b6c2d2b9e44bb91f51a
horace <hocedrix2010@gmail.com>
2024-11-30 22:55:43 +0300

    made changes to team

--- revision/settings.py
+++ revision/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
